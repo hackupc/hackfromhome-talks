29b2411b22a425051da7ed44dfa8d7cc4640193f
Albert Suarez <alsumo95@gmail.com>
2020-03-19 20:34:16 +0100

    Implemented GET/contact

--- api/src/api/contacts.py
+++ api/src/api/contacts.py
@@ -1,5 +1,19 @@
+from flask import jsonify
+
+from src.helper import response
+from src.service import contact as contact_service
+
+
 def get(contact_id=None):
-    return 'OK', 200
+    if contact_id is None:
+        contact_list = contact_service.get_all()
+        return jsonify(response.make(error=False, response=contact_list)), 200
+    else:
+        contact = contact_service.get_contact(contact_id)
+        if contact:
+            return jsonify(response.make(error=False, response=contact)), 200
+        else:
+            return jsonify(response.make(error=True, message='Contact not found.')), 200
 
 
 def post():
