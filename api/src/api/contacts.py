f52ea4a4b691d1e42e6ca22f73b1e5d59d95f19a
Albert Suarez <alsumo95@gmail.com>
2020-03-19 20:41:53 +0100

    Implemented POST/contact

--- api/src/api/contacts.py
+++ api/src/api/contacts.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request
 
 from src.helper import response
 from src.service import contact as contact_service
@@ -17,7 +17,21 @@ def get(contact_id=None):
 
 
 def post():
-    return 'OK', 201
+    request_json = request.json
+    name = response.get('name', request_json)
+    last_name = response.get('last_name', request_json)
+    image_url = response.get('image_url', request_json)
+    address = response.get('address', request_json)
+    phone_number = response.get('phone_number', request_json)
+    email = response.get('email', request_json)
+    if not all([name, last_name, image_url, address, phone_number, email]):
+        return jsonify(response.make(error=True, message='There are some required parameters missing.')), 200
+
+    contact_id = contact_service.add(name, last_name, image_url, address, phone_number, email)
+    if contact_id:
+        return jsonify(response.make(error=False, response=dict(id=contact_id))), 201
+    else:
+        return jsonify(response.make(error=True, message='Contact could not be created.')), 200
 
 
 def put(contact_id):
