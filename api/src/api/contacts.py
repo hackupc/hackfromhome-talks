301ab768f2b547819a96d794a9f79caf74abe9f6
Albert Suarez <alsumo95@gmail.com>
2020-03-19 19:53:02 +0100

    Designed all the API in OpenAPI

--- api/src/api/contacts.py
+++ api/src/api/contacts.py
@@ -1,2 +1,14 @@
 def get(contact_id=None):
     return 'OK', 200
+
+
+def post():
+    return 'OK', 201
+
+
+def put(contact_id):
+    return 'OK', 200
+
+
+def delete(contact_id):
+    return 'OK', 204
